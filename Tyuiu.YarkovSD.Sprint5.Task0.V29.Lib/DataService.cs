2596a2f3b5099f395c99f248b357851a130aff31
BorneO <savelis1908@gmail.com>
2025-11-25 13:05:47 +0300

    Добавьте файлы проекта.

---
+++ Tyuiu.YarkovSD.Sprint5.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.YarkovSD.Sprint5.Task0.V29.Lib
+{
+    public class DataService : ISprint5Task0V29
+    {
+        public string SaveToFileTextData(int x)
+        {
+            double result = 6.1 * Math.Pow(x, 3) + 0.23 * Math.Pow(x, 2) + 1.04 * x;
+            result = Math.Round(result, 3);
+            return Convert.ToString(result);
+        }
+    }
+}
