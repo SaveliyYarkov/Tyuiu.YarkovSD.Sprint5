165f0db9494941177ed425a0042f523bb8c8f5cb
BorneO <savelis1908@gmail.com>
2025-11-25 14:57:02 +0300

    Task1

---
+++ Tyuiu.YarkovSD.Sprint5.Task1.V23.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.YarkovSD.Sprint5.Task1.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
