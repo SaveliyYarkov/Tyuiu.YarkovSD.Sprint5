1fe8df498ca5d013b6904a9ca5e5a9bba1359192
BorneO <savelis1908@gmail.com>
2025-11-25 20:04:19 +0300

    Task3

--- Tyuiu.YarkovSD.Sprint5.Task3.V23.Lib/DataService.cs
+++ Tyuiu.YarkovSD.Sprint5.Task3.V23.Lib/DataService.cs
@@ -3,7 +3,7 @@ using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.YarkovSD.Sprint5.Task3.V23.Lib
 {
-    public class DataService : ISprint5Task3V22
+    public class DataService : ISprint5Task3V23
     {
         public string SaveToFileTextData(int x)
         {
