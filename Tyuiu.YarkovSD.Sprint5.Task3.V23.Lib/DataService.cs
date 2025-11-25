5c99833c8ff4e04897ddd214002dd1f158596054
BorneO <savelis1908@gmail.com>
2025-11-25 15:48:12 +0300

    Task3

--- Tyuiu.YarkovSD.Sprint5.Task3.V23.Lib/DataService.cs
+++ Tyuiu.YarkovSD.Sprint5.Task3.V23.Lib/DataService.cs
@@ -1,29 +1,28 @@
 ﻿using System;
 using System.IO;
 using tyuiu.cources.programming.interfaces.Sprint5;
-
 namespace Tyuiu.YarkovSD.Sprint5.Task3.V23.Lib
 {
     public class DataService : ISprint5Task3V23
     {
         public string SaveToFileTextData(int x)
         {
+            string i = "";
+            //string outputFile = "OutPutFileTask3.bin";
+            //string path = Path.Combine(Path.GetTempPath(), outputFile);
 
-            string outputFile = "OutPutFileTask3.bin";
-            string path = Path.Combine(Path.GetTempPath(), outputFile);
+            //FileInfo fileInfo = new FileInfo(path);
+            //if (fileInfo.Exists)
+                //File.Delete(path);
 
-            FileInfo fileInfo = new FileInfo(path);
-            if (fileInfo.Exists)
-                File.Delete(path);
+            //double result = (1 + Math.Pow(x, 3)) / x;
+            //double r = Math.Round(result, 3);
 
-            double result = (1 + Math.Pow(x, 3)) / x;
-            double r = Math.Round(result, 3);
-
-            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
-            {
-                writer.Write(r);
-            }
-            return path;
+           // using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
+            //{
+               // writer.Write(r);
+            //}
+            return i;
         }
     }
 }
