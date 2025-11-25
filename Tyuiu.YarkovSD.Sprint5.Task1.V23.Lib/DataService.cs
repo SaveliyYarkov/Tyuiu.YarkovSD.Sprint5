d3451057cc442894af2b908dfc39eb47b8499ef7
BorneO <savelis1908@gmail.com>
2025-11-25 14:29:47 +0300

    Task1

---
+++ Tyuiu.YarkovSD.Sprint5.Task1.V23.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.YarkovSD.Sprint5.Task1.V23.Lib
+{
+    public class DataService : ISprint5Task1V23
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string a = "";
+
+            for (double x = startValue; x <= stopValue; x += 1)
+            {
+                double result;
+                if (x + 2 == 0)
+                {
+                    result = 0;
+                }
+                else
+                {
+                    result = Math.Cos(x) + (Math.Cos(x) / (x + 2)) - 3 * x;
+                }
+
+                result = Math.Round(result, 2);
+                a += Convert.ToString(result);
+            }
+
+            return a;
+        }
+    }
+}
