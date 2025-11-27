6d060ffb472644aa11ca8726abaa93c13cf45e2a
BorneO <savelis1908@gmail.com>
2025-11-27 12:16:16 +0300

    Task4

---
+++ Tyuiu.YarkovSD.Sprint5.Task4.V23.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.YarkovSD.Sprint5.Task4.V23.Lib
+{
+    public class DataService : ISprint5Task4V23
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double a = Convert.ToDouble(File.ReadAllText(path).Replace(".", ","));
+
+            return Math.Round(Math.Pow(a, -3) + 2 + Math.Cos(a), 3);
+        }
+    }
+}
