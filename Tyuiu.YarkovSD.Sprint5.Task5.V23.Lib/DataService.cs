c0598085ea62327b30b80cd9a29f7e10c3a385a4
BorneO <savelis1908@gmail.com>
2025-11-27 12:34:13 +0300

    Task5

---
+++ Tyuiu.YarkovSD.Sprint5.Task5.V23.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.YarkovSD.Sprint5.Task5.V23.Lib
+{
+    public class DataService : ISprint5Task5V23
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string text = File.ReadAllText(path);
+            string[] numberStrings = text.Split(',');
+
+            double minValue = double.MaxValue;
+
+            foreach (string numberStr in numberStrings)
+            {
+                if (double.TryParse(numberStr.Trim(), out double number))
+                {
+                    if (number < minValue)
+                    {
+                        minValue = number;
+                    }
+                }
+            }
+            return Math.Round(minValue, 3);
+        }
+    }
+}
