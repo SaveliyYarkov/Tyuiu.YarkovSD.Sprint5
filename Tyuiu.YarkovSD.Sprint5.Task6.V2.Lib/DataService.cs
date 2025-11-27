4de6b4ff4951e5f5a28a9150bb6b0893f67dc1ac
BorneO <savelis1908@gmail.com>
2025-11-27 13:16:08 +0300

    Task6

--- Tyuiu.YarkovSD.Sprint5.Task6.V2.Lib/DataService.cs
+++ Tyuiu.YarkovSD.Sprint5.Task6.V2.Lib/DataService.cs
@@ -1,15 +1,15 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.YarkovSD.Sprint5.Task6.V2.Lib
 {
-    public class DataSevice : ISprint5Task6V2
+    public class DataService : ISprint5Task6V2
     {
         public int LoadFromDataFile(string path)
         {
-
-            // Используем регулярное выражение для поиска русских букв
-            int count = Regex.Matches(path, @"[А-Яа-яЁё]").Count;
+            string content = File.ReadAllText(path, Encoding.UTF8);
+            int count = Regex.Matches(content, @"[А-Яа-яЁё]").Count;
 
             return count;
         }
