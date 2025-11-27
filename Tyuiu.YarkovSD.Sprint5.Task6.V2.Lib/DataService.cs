0fe245ccb56a774ee82a733381b78de4827fb93d
BorneO <savelis1908@gmail.com>
2025-11-27 13:11:32 +0300

    Task6

---
+++ Tyuiu.YarkovSD.Sprint5.Task6.V2.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.YarkovSD.Sprint5.Task6.V2.Lib
+{
+    public class DataSevice : ISprint5Task6V2
+    {
+        public int LoadFromDataFile(string path)
+        {
+
+            // Используем регулярное выражение для поиска русских букв
+            int count = Regex.Matches(path, @"[А-Яа-яЁё]").Count;
+
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.YarkovSD.Sprint5.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.YarkovSD.Sprint5.Task6.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
